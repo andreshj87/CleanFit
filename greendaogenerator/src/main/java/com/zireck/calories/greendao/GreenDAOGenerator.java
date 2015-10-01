f1a5185b787a4f13b1f35bbad20aa53f33a51cff
Zireck <andreshj87@gmail.com>
2015-10-01 14:06:16 +0200

    Finally the project is fully renamed.

--- greendaogenerator/src/main/java/com/zireck/calories/greendao/GreenDAOGenerator.java
+++ greendaogenerator/src/main/java/com/zireck/calories/greendao/GreenDAOGenerator.java
@@ -8,7 +8,7 @@ import de.greenrobot.daogenerator.Schema;
 public class GreenDAOGenerator {
 
     public static void main(String args[]) throws Exception {
-        Schema schema = new Schema(1, "com.zireck.projectk.data.entity");
+        Schema schema = new Schema(1, "com.zireck.calories.data.entity");
 
         Entity food = addFood(schema);
         addMeal(schema, food);
